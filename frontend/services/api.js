33b3c2c17da0441fed74634c1e04bf1e2f89fd34
Sway-Nick <sway1976@gmail.com>
2026-08-02 16:56:10 +0300

    Connect frontend with vocabulary API

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,14 +1,16 @@
 const API_URL =
   'https://script.google.com/macros/s/AKfycby0lLhpcGJOddZ6L64_D5i14zcU1ZdCtkgA3sj1G9w36eelkGPP4M6k2iTZekTGFAHhFg/exec';
 
-async function checkHealth() {
+async function getHealth() {
   const response = await fetch(`${API_URL}?route=health`);
 
-  const data = await response.json();
-
-  console.log(data);
-
-  document.querySelector('#status').textContent = data.data.status;
+  return response.json();
 }
 
-checkHealth();
+async function getWords() {
+  const response = await fetch(`${API_URL}?route=words`);
+
+  return response.json();
+}
+
+export { getHealth, getWords };
